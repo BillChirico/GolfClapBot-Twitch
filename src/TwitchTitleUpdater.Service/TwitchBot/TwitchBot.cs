bc8ad057f4d7cbf968d0a753a7028335970e46d2
Bill Chirico <billchirico@gmail.com>
2019-02-21 21:33:26 -0500

    Add DI to TwitchBot

--- src/TwitchTitleUpdater.Service/TwitchBot/TwitchBot.cs
+++ src/TwitchTitleUpdater.Service/TwitchBot/TwitchBot.cs
@@ -1,10 +1,18 @@
 using System;
 using System.Threading.Tasks;
+using TwitchLib.Client.Interfaces;
 
 namespace TwitchTitleUpdater.Service.TwitchBot
 {
     public class TwitchBot : ITwitchBot
     {
+        private readonly ITwitchClient _twitchClient;
+
+        public TwitchBot(ITwitchClient twitchClient)
+        {
+            _twitchClient = twitchClient;
+        }
+        
         public Task Connect(string clientId, string accessToken)
         {
             throw new NotImplementedException();
