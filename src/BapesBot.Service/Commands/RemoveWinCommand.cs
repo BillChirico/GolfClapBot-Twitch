beab69286bc73cb8087eaed5f4c39a71240c8f1f
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2019-02-24 21:18:34 -0500

    Add RemoveWinCommand to remove win from win count

---
+++ src/BapesBot.Service/Commands/RemoveWinCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Interfaces;
+
+namespace BapesBot.Service.Commands
+{
+    public class RemoveWinCommand : ICommand
+    {
+        private readonly ITwitchClient _twitchClient;
+
+        public RemoveWinCommand(ITwitchClient twitchClient) : base("rmwin")
+        {
+            _twitchClient = twitchClient;
+        }
+
+        public override Task<bool> Invoke(OnMessageReceivedArgs message)
+        {
+
+            if (AddWinCommand.Wins <= 0)
+            {
+                _twitchClient.SendMessage(message.ChatMessage.Channel, $"No wins to remove");
+            }
+
+            else
+            {
+                AddWinCommand.Wins -= 1;
+                _twitchClient.SendMessage(message.ChatMessage.Channel, $"Win Removed. Wins now: {AddWinCommand.Wins}");
+            }
+
+            return Task.FromResult(true);
+        }
+    }
+}
