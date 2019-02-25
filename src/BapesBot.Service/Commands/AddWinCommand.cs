b5dff566cad8f36ae4775f65a83f32b030b21b86
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2019-02-24 21:17:57 -0500

    Add AddWinCommand to increase win count

---
+++ src/BapesBot.Service/Commands/AddWinCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Interfaces;
+
+
+namespace BapesBot.Service.Commands
+{
+    public class AddWinCommand : ICommand
+    {
+        private readonly ITwitchClient _twitchClient;
+
+        public AddWinCommand(ITwitchClient twitchClient) : base("addwin")
+        {
+            _twitchClient = twitchClient;
+        }
+
+        public static int Wins { get; set; }
+
+
+        public override Task<bool> Invoke(OnMessageReceivedArgs message)
+        {
+            Wins += 1;
+            _twitchClient.SendMessage(message.ChatMessage.Channel, $"Win Added. Current Wins:{Wins}");
+
+            return Task.FromResult(true);
+        }
+
+        public static implicit operator int(AddWinCommand v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
