e46849090cc69b422d198ec703e6c00bce0b65b3
Bill Chirico <billchirico@gmail.com>
2019-04-09 00:17:41 -0400

    Get wins for current game

--- src/GolfClapBot.Service/Commands/GetWinsCommand.cs
+++ src/GolfClapBot.Service/Commands/GetWinsCommand.cs
@@ -1,34 +1,40 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using GolfClapBot.Service.Counter;
+using GolfClapBot.Service.TwitchApiHelper;
 using TwitchLib.Client.Interfaces;
 using TwitchLib.Client.Models;
 
 namespace GolfClapBot.Service.Commands
 {
     /// <summary>
-    ///     Displays current wins.
+    ///     Displays current wins for the current game.
     /// </summary>
     public class GetWinsCommand : Command
     {
         private readonly ICounterService _counterService;
+        private readonly ITwitchApiHelper _twitchApiHelper;
         private readonly ITwitchClient _twitchClient;
 
-        public GetWinsCommand(ITwitchClient twitchClient, ICounterService counterService) : base(new List<string>
+        public GetWinsCommand(ITwitchClient twitchClient, ICounterService counterService,
+            ITwitchApiHelper twitchApiHelper) : base(new List<string>
             {"wins"})
         {
             _twitchClient = twitchClient;
             _counterService = counterService;
+            _twitchApiHelper = twitchApiHelper;
         }
 
-        public override Task<bool> Invoke(ChatMessage message)
+        public override async Task<bool> Invoke(ChatMessage message)
         {
-            var counter = _counterService.GetCounter("Wins");
+            var game = (await _twitchApiHelper.GetStreamGame(message.Channel)).Name;
+
+            var counter = _counterService.GetCounter($"{game}Wins");
 
             _twitchClient.SendMessage(message.Channel,
-                counter != null ? $"Current Wins: {counter.Count}" : "Current Wins: 0");
+                counter == null ? $"There are no wins recorded for {game}!" : $"Current {game} Wins: {counter.Count}");
 
-            return Task.FromResult(true);
+            return await Task.FromResult(true);
         }
     }
 }
