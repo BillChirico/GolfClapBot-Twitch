919adc7a43e41750dd85b3421ee68eeadfc9382d
Bill Chirico <billchirico@gmail.com>
2019-04-14 22:30:05 -0400

    Remove channel settings

--- src/GolfClapBot.Domain/Settings/GolfClapBotSettings.cs
+++ src/GolfClapBot.Domain/Settings/GolfClapBotSettings.cs
@@ -2,8 +2,6 @@ namespace GolfClapBot.Domain.Settings
 {
     public class GolfClapBotSettings
     {
-        public string Channel { get; set; }
-
         public bool EnableSoundEffects { get; set; }
     }
 }
