09f93dd8c753f1c7b298f5a12db2a6d03e59539f
Bill Chirico <billchirico@gmail.com>
2019-04-24 00:20:47 -0400

    Add womp sound effect

---
+++ src/GolfClapBot.Service/SoundEffects/Womp.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace GolfClapBot.Service.SoundEffects
+{
+    /// <summary>
+    ///     Play the womp sound effect.
+    /// </summary>
+    public class Womp : SoundEffect
+    {
+        public Womp() : base(new List<string> {"thebapWomp", "!womp", "womp"},
+            "./SoundFiles/Womp.wav")
+        {
+        }
+    }
+}
